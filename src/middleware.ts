d45641020f39a2ed03637519cdd79e06846bc4d1
Mohammad Ridwanur Rahman <ridwanur13@gmail.com>
2025-08-27 19:44:55 +0600

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,5 @@
 // middleware.ts
-import { NextResponse } from "next/server";
-import { auth } from "@/auth";
-
-export async function middleware(req: Request) {
-  const session = await auth();
-  if (!session?.user) {
-    const url = new URL("/sign-in", req.url);
-    url.searchParams.set("callbackUrl", new URL(req.url).pathname);
-    return NextResponse.redirect(url);
-  }
-  return NextResponse.next();
-}
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/dashboard/:path*"],
